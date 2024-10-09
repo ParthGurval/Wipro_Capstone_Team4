adb70bb85c6eac139bf49c00b2da07904fd6ee54
ParthGurval <sohamgurval2001@gmail.com>
2024-10-09 15:05:20 +0530

    FarmPartner and OrderDetails Service

---
+++ Organic_Farm/farmPartner/src/main/java/com/wipro/cpastone/farmpartner/FarmPartnerApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.cpastone.farmpartner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmPartnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmPartnerApplication.class, args);
+	}
+
+}
